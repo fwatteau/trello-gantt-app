d40f1e33c701ecf69aade3f94f65577885ae5341
Kef <f.watteau@gmail.com>
2019-02-27 22:18:31 +0100

    TEST : Variable d'environnement netlify ko

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -37,7 +37,6 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    console.log(`{$process.env.TRELLO_API_KEY}`, `{process.env.TRELLO_API_KEY}`);
     this.trelloService.authorize();
     // Initialisation du Gantt
     this.initGantt();
